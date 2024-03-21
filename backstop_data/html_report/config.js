ba9117c515c2573d5c9b5389bf285f63d868e210
Anastasiya Kubatina <82117054+kybikn@users.noreply.github.com>
2024-03-21 23:23:16 +0300

    feat: сделано задание форма

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,13 +3,13 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "../../test-js/component-1/reference/accordeon_21.png",
-        "test": "../bitmaps_test/20240321-024443/accordeon_21.png",
+        "reference": "../../test-js/component-1/reference/accordeon_31.png",
+        "test": "../bitmaps_test/20240321-231502/accordeon_31.png",
         "selector": "document",
-        "fileName": "accordeon_21.png",
-        "label": "accordeon 2.1",
+        "fileName": "accordeon_31.png",
+        "label": "accordeon 3.1",
         "misMatchThreshold": 4,
-        "url": "file:///Users/kybikn/Documents/Programming/Проекты/task-test/task-js/component-1/index.html?tabs_limit=1",
+        "url": "file:///Users/kybikn/Documents/Programming/Проекты/task-test/task-js/component-1/index.html?tabs_limit=3",
         "expect": 0,
         "viewportLabel": "desktop",
         "diff": {
@@ -20,20 +20,20 @@ report({
           },
           "rawMisMatchPercentage": 0,
           "misMatchPercentage": "0.00",
-          "analysisTime": 46
+          "analysisTime": 137
         }
       },
       "status": "pass"
     },
     {
       "pair": {
-        "reference": "../../test-js/component-1/reference/accordeon_22.png",
-        "test": "../bitmaps_test/20240321-024443/accordeon_22.png",
+        "reference": "../../test-js/component-1/reference/accordeon_32.png",
+        "test": "../bitmaps_test/20240321-231502/accordeon_32.png",
         "selector": "document",
-        "fileName": "accordeon_22.png",
-        "label": "accordeon 2.2",
+        "fileName": "accordeon_32.png",
+        "label": "accordeon 3.2",
         "misMatchThreshold": 4,
-        "url": "file:///Users/kybikn/Documents/Programming/Проекты/task-test/task-js/component-1/index.html?tabs_limit=1",
+        "url": "file:///Users/kybikn/Documents/Programming/Проекты/task-test/task-js/component-1/index.html?tabs_limit=3",
         "expect": 0,
         "viewportLabel": "desktop",
         "diff": {
@@ -44,20 +44,20 @@ report({
           },
           "rawMisMatchPercentage": 0,
           "misMatchPercentage": "0.00",
-          "analysisTime": 47
+          "analysisTime": 115
         }
       },
       "status": "pass"
     },
     {
       "pair": {
-        "reference": "../../test-js/component-1/reference/accordeon_23.png",
-        "test": "../bitmaps_test/20240321-024443/accordeon_23.png",
+        "reference": "../../test-js/component-1/reference/accordeon_33.png",
+        "test": "../bitmaps_test/20240321-231502/accordeon_33.png",
         "selector": "document",
-        "fileName": "accordeon_23.png",
-        "label": "accordeon 2.3",
+        "fileName": "accordeon_33.png",
+        "label": "accordeon 3.3",
         "misMatchThreshold": 4,
-        "url": "file:///Users/kybikn/Documents/Programming/Проекты/task-test/task-js/component-1/index.html?tabs_limit=1",
+        "url": "file:///Users/kybikn/Documents/Programming/Проекты/task-test/task-js/component-1/index.html?tabs_limit=3",
         "expect": 0,
         "viewportLabel": "desktop",
         "diff": {
@@ -68,7 +68,31 @@ report({
           },
           "rawMisMatchPercentage": 0,
           "misMatchPercentage": "0.00",
-          "analysisTime": 43
+          "analysisTime": 124
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../../test-js/component-1/reference/accordeon_34.png",
+        "test": "../bitmaps_test/20240321-231502/accordeon_34.png",
+        "selector": "document",
+        "fileName": "accordeon_34.png",
+        "label": "accordeon 3.4",
+        "misMatchThreshold": 4,
+        "url": "file:///Users/kybikn/Documents/Programming/Проекты/task-test/task-js/component-1/index.html?tabs_limit=3",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 128
         }
       },
       "status": "pass"
--- task-js/component-2/js/script.js
+++ task-js/component-2/js/script.js
@@ -1,5 +1,221 @@
-(function() {
+(function () {
   /* Код компонента пишите здесь */
+  const form = document.getElementById('booking-form');
+  const inputPhone = document.getElementById('phone');
+  const inputCheckin = document.getElementById('checkin-date');
+  const inputCheckout = document.getElementById('checkout-date');
+  const inputAdults = document.getElementById('adults');
+  const inputChildren = document.getElementById('children');
+
+  function errorInput(input) {
+    input.classList.remove('field-correct');
+    input.classList.add('field-error');
+    return
+  }
+
+  function goodInput(input) {
+    input.classList.remove('field-error');
+    input.classList.add('field-correct');
+    return
+  }
+
+  // проверка телефона
+  function checkPhone() {
+    // поле не пустое
+    if (!inputPhone.value) {
+      console.log('номер не должен быть пустым')
+      errorInput(inputPhone);
+      return
+    }
+    // поле начинается с +7
+    if (!inputPhone.value.startsWith('+7')) {
+      console.log('номер должен начинаться с +7')
+      errorInput(inputPhone);
+      return
+    }
+    const allowedSymbols = ['(', ')', '-', ' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let numbersCount = 0;
+    let countErrors = 0;
+    inputPhone.value.slice(2).split('').forEach(symbol => {
+      if (!allowedSymbols.includes(symbol)) {
+        countErrors++
+      }
+      if (numbers.includes(symbol)) { numbersCount++ }
+    });
+    // есть только разрешенные символы
+    if (countErrors > 0) {
+      console.log('в номере недопустимые символы')
+      errorInput(inputPhone);
+      return
+    }
+    // есть 10 цифр после +7
+    if (numbersCount !== 10) {
+      console.log('в номере должно быть 10 цифр после +7')
+      errorInput(inputPhone);
+      return
+    }
+    // тогда все хорошо
+    goodInput(inputPhone);
+  }
 
 
+  // проверка дат
+  function checkDate() {
+    // Даты не пустые
+    if (!inputCheckin.value || !inputCheckout.value) {
+      inputCheckin.classList.add('field-error');
+      console.log('поля дат должны быть не пустые')
+      errorInput(inputCheckin);
+      errorInput(inputCheckout);
+      return
+    }
+
+    // Даты должны быть в формате ГГГГ-ММ-ДД или ДД.ММ.ГГГГ
+    function makeDate(dateString) {
+      let date = NaN;
+      let regex1 = /(\d{4}-\d{2}-\d{2})/gm;
+      let regex2 = /(\d{2}\.\d{2}\.\d{4})/gm;
+      if (regex1.test(dateString)) {
+        date = new Date(dateString)
+      } else if (regex2.test(dateString)) {
+        const [dd, mm, yyyy] = dateString.split('.');
+        date = new Date(`${yyyy}-${mm}-${dd}`);
+      } else {
+        console.log('дата должна быть в формате ГГГГ-ММ-ДД или ДД.ММ.ГГГГ')
+      }
+      return date
+    }
+
+    // Даты должны быть валидные
+    const checkInDate = makeDate(inputCheckin.value.trim());
+    const checkOutDate = makeDate(inputCheckout.value.trim());
+
+    if (isNaN(checkInDate) || isNaN(checkOutDate)) {
+      console.log('такой даты не существует, дата должна быть в формате ГГГГ-ММ-ДД или ДД.ММ.ГГГГ');
+      errorInput(inputCheckin);
+      errorInput(inputCheckout);
+      return
+    }
+
+    // Дата выезда должна быть больше даты заезда минимум на 4 дня
+    const timeDifference = checkOutDate.getTime() - checkInDate.getTime();
+    const daysDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
+    // console.log('daysDifference:', daysDifference);
+    if (daysDifference < 4) {
+      console.log('Дата выезда должна быть больше даты заезда минимум на 4 дня.')
+      errorInput(inputCheckin);
+      errorInput(inputCheckout);
+      return
+    }
+    goodInput(inputCheckin);
+    goodInput(inputCheckout);
+  }
+
+  // проверка людей
+  function checkPeople() {
+
+    // количество взрослых должно быть хотя бы 1. 
+    if (!inputAdults.value || parseInt(inputAdults.value) < 1) {
+      console.log('Количество взрослых должно быть хотя бы 1.')
+      errorInput(inputAdults);
+      errorInput(inputChildren);
+      return
+    }
+
+    // количество детей не может быть больше количества взрослых.
+    if (parseInt(inputChildren.value) > parseInt(inputAdults.value)) {
+      console.log('Количество детей не может быть больше количества взрослых.')
+      errorInput(inputAdults);
+      errorInput(inputChildren);
+      return
+    }
+
+    // если выбран одноместный номер, количество взрослых не может превышать 1.
+    if (document.getElementById('radio-1').checked) {
+      if (parseInt(inputAdults.value) > 1) {
+        console.log('Если выбран одноместный номер, количество взрослых не может превышать 1.')
+        errorInput(inputAdults);
+        errorInput(inputChildren);
+        return
+      }
+    }
+
+    //Если выбран семейный номер, количество взрослых должно быть 2 или
+    // более, а количество детей — хотя бы 1. 
+    if (document.getElementById('radio-3').checked) {
+      if (!(parseInt(inputAdults.value) >= 2 && parseInt(inputChildren.value) >= 1)) {
+        console.log('Если выбран семейный номер, количество взрослых должно быть 2 или более, а количество детей — хотя бы 1')
+        errorInput(inputAdults);
+        errorInput(inputChildren);
+        return
+      }
+    }
+
+    goodInput(inputAdults);
+    goodInput(inputChildren);
+  }
+
+  // главная функция-обработчик
+  function checkInputValidity() {
+    checkPhone();
+    checkDate();
+    checkPeople();
+  }
+
+  form.addEventListener('submit', (e) => {
+    console.log('hi');
+    e.preventDefault();
+    checkInputValidity();
+  })
+
+  form.submit();
 })();
+
+// console.log('form:', form);
+// const formInput = document.querySelector('.field-input');
+// console.log('formInput:', formInput);
+// const formInputs = document.querySelectorAll('.field-input');
+// console.log('formInputs:', formInputs);
+// const phoneInput = form.querySelectorById('#phone');
+// const inputList = Array.from(document.querySelectorAll('.field-input'));
+// console.log('inputList:', inputList);
+// const phone = inputPhone.value;
+
+// function setEventListeners() {
+// inputList.forEach(formInputs) {
+//   formInputs.addEventListener("input", () => {
+//     checkInputValidity(formInputs);
+//   });
+// }
+
+// inputList.forEach((formInput) => {
+//   console.log('formInput', formInput);
+//   inputList.addEventListener('input', checkInputValidity());
+// })
+
+// inputList.forEach(function (formInput) {
+//   checkInputValidity(formInput)
+// inputList.addEventListener('input', checkInputValidity());
+// });
+
+// inputDate.addEventListener('input', checkInputValidity)
+
+// const inputValue = formInput.value;
+// inputList.map(inputValue => inputValue.value)
+// inputList.forEach(formInput)
+// console.log('inputList:', inputList);
+// if (!inputList.validity.valid) {
+// if (!inputValue.validity.valid) {
+// if (!inputList.value) {
+// if (!inputDate.value || phone || date) {
+// if (!inputPhone.value) {
+// console.log('inputInput.value:', inputValue);
+// formInput.classList.add('field-error');
+// inputList.classList.add('field-error');
+// form.classList.add('field-error');
+// } else {
+// inputList.classList.remove('field-error');
+// form.classList.remove('field-error');
+//   formInput.classList.remove('field-error');
+// }
